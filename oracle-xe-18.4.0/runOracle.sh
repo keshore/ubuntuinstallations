608f2f2162a9c6a00a04871ea8a52d11873ab149
Keshore Durairaj <keshore12@gmail.com>
2020-07-29 23:18:16 +0530

    updated oracle run step

--- oracle-xe-18.4.0/runOracle.sh
+++ oracle-xe-18.4.0/runOracle.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo mkdir -p /oradata && sudo chmod 777 /oradata
 docker run  \
+-d \
 -p 1521:1521 \
 -p 5050:5500 \
 -v /oradata:/opt/oracle/oradata \
+--restart=unless-stopped \
 oracle/database:18.4.0-xe
